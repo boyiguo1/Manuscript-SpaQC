9d2cf7a38090517b5acecaa16a58f0512403c913
Michael Totty <mictott@gmail.com>
2023-11-07 12:41:23 -0500

    reran SpotSweeper_testing with new scales and corrected boxplots.

--- code/single_cell_methods/01_singlecell_qc.R
+++ code/single_cell_methods/01_singlecell_qc.R
@@ -297,21 +297,21 @@ layer_guess_ordered_col <- spe$layer_guess_reordered
 discarded_indices <- which(discard_new_col == TRUE)
 discarded_domains <- layer_guess_ordered_col[discarded_indices]
 
-# Count the occurrences of each spatial domain for the discarded spots
-domain_counts <- table(discarded_domains)
 
-# Convert to percentages
-domain_percentages <- (domain_counts / sum(domain_counts)) * 100
+# Count the occurrences of each spatial domain for the discarded spots
+domain_discarded_counts <- table(discarded_domains)
+domain_total_counts <- table(layer_guess_ordered_col)
+discarded_percentages <- (domain_discarded_counts/domain_total_counts)*100
 
 # Convert the domain counts to a data frame
-df <- as.data.frame(domain_percentages)
+df <- as.data.frame(discarded_percentages)
 colnames(df) <- c("SpatialDomain", "Percentage")
 
 # Plot using ggplot2
 p <- ggplot(df, aes(x = SpatialDomain, y = Percentage, fill = SpatialDomain)) +
   geom_bar(stat = "identity") +
   scale_fill_manual(values = spatialLIBD::libd_layer_colors) +
-  labs(title = "Percentage of Discarded Spots by Spatial Domain",
+  labs(title = "Average Percentage of Discarded Spots for each Spatial Domain",
        x = "Spatial Domain",
        y = "Percentage") +
   theme_minimal() +
@@ -320,7 +320,6 @@ p <- ggplot(df, aes(x = SpatialDomain, y = Percentage, fill = SpatialDomain)) +
 pdf(width=6, height=3,here(plot_dir, "Barplot_discarded_by_layer.pdf"))
 p
 dev.off()
-
 
 
 # ===== Average percentage across samples =====
@@ -337,33 +336,26 @@ df <- data.frame(discard_new = discard_new_col,
 df <- df[!is.na(df$layer_guess_ordered), ]
 
 
-# Assuming discarded_df has a logical column 'discard_new' where TRUE indicates a discarded spot
 summary_df <- df %>%
   group_by(sample_id, layer_guess_ordered) %>%
   summarise(
     total_spots = n(),  # Total number of spots in each domain for each sample
     discarded_spots = sum(discard_new, na.rm = TRUE),  # Counting TRUE as 1 and FALSE as 0
-    .groups = 'drop'  # Dropping the grouping after summarise
+    .groups = 'keep'  # Dropping the grouping after summarise
   ) %>%
   mutate(
     percentage_discarded = (discarded_spots / total_spots) * 100  # Calculating the percentage
-  )
+  ) 
 
 
-# Create a dataframe with every possible combination of sample_id and layer_guess_ordered
-all_combinations <- expand(df, sample_id, layer_guess_ordered)
-
-# Left join summary_df onto all_combinations, filling in missing values with 0
-final_df <- left_join(all_combinations, summary_df, by = c("sample_id", "layer_guess_ordered")) %>%
-  replace_na(list(n = 0, percentage = 0))
 
 # Plot using ggplot2
-p <- ggplot(final_df, aes(x = layer_guess_ordered, y = percentage, fill = layer_guess_ordered)) +
+p <- ggplot(summary_df, aes(x = layer_guess_ordered, y = percentage_discarded, fill = layer_guess_ordered)) +
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
   geom_jitter(width = 0.2, size = 2, aes(color = layer_guess_ordered), alpha = 0.7) +  # Display all data points with matching layer colors
   scale_fill_manual(values = spatialLIBD::libd_layer_colors) +
   scale_color_manual(values = spatialLIBD::libd_layer_colors) +
-  labs(title = "Percentage of Discarded Spots by Spatial Domain for each Sample ID",
+  labs(title = "Percentage of Discarded Spots within each Spatial Domain",
        x = "Spatial Domain",
        y = "Percentage") +
   theme_minimal() +
@@ -372,4 +364,6 @@ p <- ggplot(final_df, aes(x = layer_guess_ordered, y = percentage, fill = layer_
 pdf(width=8, height=4,here(plot_dir, "Boxplot_discarded_by_layer.pdf"))
 p
 dev.off()
+
+
 
--- code/spatial_methods/03_SpotSweeper_testing.R
+++ code/spatial_methods/03_SpotSweeper_testing.R
@@ -39,7 +39,7 @@ spe.subset <- subset(spe, ,sample_id == spe$sample_id[1])
 # Visualize
 p1 <- make_escheR(spe.subset) |> 
   add_fill(var = "sum_umi_log2") +
-  scale_fill_gradient2(low ="purple" , mid = "white",high =  "darkgreen")
+  scale_fill_gradient(low ="white",high =  "darkgreen")
 
 p2 <- make_escheR(spe.subset) |> 
   add_fill(var = "z.umi") +
@@ -54,7 +54,7 @@ p3 <- make_escheR(spe.subset) |>
       "TRUE" = "red",
       "FALSE" = "transparent")
   ) +
-  scale_fill_gradient2(low ="purple" , mid = "white",high =  "darkgreen")
+  scale_fill_gradient(low ="white",high =  "darkgreen")
 
 p4 <- make_escheR(spe.subset) |> 
   add_fill(var = "z.umi") |>
@@ -73,43 +73,6 @@ dev.off()
 
 # ====================== Explore these outliers ======================
 
-
-# ==== Total percentage =====
-# Extract the required columns from the spe object
-discard_new_col <- spe$z_umi_outlier
-layer_guess_ordered_col <- spe$layer_guess_reordered
-
-# Filter out the discarded spots
-discarded_indices <- which(discard_new_col == TRUE)
-discarded_domains <- layer_guess_ordered_col[discarded_indices]
-
-# Count the occurrences of each spatial domain for the discarded spots
-domain_counts <- table(discarded_domains)
-
-# Convert to percentages
-domain_percentages <- (domain_counts / sum(domain_counts)) * 100
-
-# Convert the domain counts to a data frame
-df <- as.data.frame(domain_percentages)
-colnames(df) <- c("SpatialDomain", "Percentage")
-
-# Plot using ggplot2
-p <- ggplot(df, aes(x = SpatialDomain, y = Percentage, fill = SpatialDomain)) +
-  geom_bar(stat = "identity") +
-  scale_fill_manual(values = spatialLIBD::libd_layer_colors) +
-  labs(title = "Percentage of Discarded Spots by Spatial Domain",
-       x = "Spatial Domain",
-       y = "Percentage") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-pdf(width=6, height=3,here(plot_dir, "Barplot_discarded_by_layer_k6.pdf"))
-p
-dev.off()
-
-
-
-
 # ===== Average percentage across samples =====
 # Extract the required columns
 discard_new_col <- spe$z_umi_outlier
@@ -123,45 +86,38 @@ df <- data.frame(discard_new = discard_new_col,
 
 df <- df[!is.na(df$layer_guess_ordered), ]
 
-# Filter out the discarded spots
-discarded_df <- df[df$discard_new == TRUE, ]
 
-# Group by sample_id and layer_guess_ordered, then calculate percentages
-summary_df <- discarded_df %>%
+summary_df <- df %>%
   group_by(sample_id, layer_guess_ordered) %>%
-  summarise(n = n()) %>%
-  mutate(percentage = (n / sum(n)) * 100)
-
-# Create a dataframe with every possible combination of sample_id and layer_guess_ordered
-all_combinations <- df %>% 
-  tidyr::expand(sample_id, layer_guess_ordered)
-
-
-# Left join summary_df onto all_combinations, filling in missing values with 0
-final_df <- left_join(all_combinations, summary_df, by = c("sample_id", "layer_guess_ordered")) %>%
-  replace_na(list(n = 0, percentage = 0))
+  summarise(
+    total_spots = n(),  # Total number of spots in each domain for each sample
+    discarded_spots = sum(discard_new, na.rm = TRUE),  # Counting TRUE as 1 and FALSE as 0
+    .groups = 'keep'  # Dropping the grouping after summarise
+  ) %>%
+  mutate(
+    percentage_discarded = (discarded_spots / total_spots) * 100  # Calculating the percentage
+  ) 
 
 # Plot using ggplot2
-p <- ggplot(final_df, aes(x = layer_guess_ordered, y = percentage, fill = layer_guess_ordered)) +
+p <- ggplot(summary_df, aes(x = layer_guess_ordered, y = percentage_discarded, fill = layer_guess_ordered)) +
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
   geom_jitter(width = 0.2, size = 2, aes(color = layer_guess_ordered), alpha = 0.7) +  # Display all data points with matching layer colors
   scale_fill_manual(values = spatialLIBD::libd_layer_colors) +
   scale_color_manual(values = spatialLIBD::libd_layer_colors) +
-  labs(title = "Percentage of Discarded Spots by Spatial Domain for each Sample ID",
+  labs(title = "Percentage of Discarded Spots within each Spatial Domain",
        x = "Spatial Domain",
        y = "Percentage") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-pdf(width=8, height=4,here(plot_dir, "Boxplot_discarded_by_layer_by_sample_k6.pdf"))
+pdf(width=8, height=4,here(plot_dir, "Boxplot_percent_discarded_k6.pdf"))
 p
 dev.off()
 
-# ============ Testing different Ks ==========
-# ====================== Testing with my newly made function ======================
 
-# I turned the above code into functions in the private SpotSweeper package
-# https://github.com/MicTott/SpotSweeper
+
+# ============ Testing different Ks ==========
+
 
 spe <- localOutliers(spe, k= 18, threshold=3)
 
@@ -172,7 +128,7 @@ spe.subset <- subset(spe, ,sample_id == spe$sample_id[1])
 # Visualize
 p1 <- make_escheR(spe.subset) |> 
   add_fill(var = "sum_umi_log2") +
-  scale_fill_gradient2(low ="purple" , mid = "white",high =  "darkgreen")
+  scale_fill_gradient(low ="white",high =  "darkgreen")
 
 p2 <- make_escheR(spe.subset) |> 
   add_fill(var = "z.umi") +
@@ -187,7 +143,7 @@ p3 <- make_escheR(spe.subset) |>
       "TRUE" = "red",
       "FALSE" = "transparent")
   ) +
-  scale_fill_gradient2(low ="purple" , mid = "white",high =  "darkgreen")
+  scale_fill_gradient(low ="white",high =  "darkgreen")
 
 p4 <- make_escheR(spe.subset) |> 
   add_fill(var = "z.umi") |>
@@ -208,42 +164,6 @@ dev.off()
 
 
 # ==== Total percentage =====
-# Extract the required columns from the spe object
-discard_new_col <- spe$z_umi_outlier
-layer_guess_ordered_col <- spe$layer_guess_reordered
-
-# Filter out the discarded spots
-discarded_indices <- which(discard_new_col == TRUE)
-discarded_domains <- layer_guess_ordered_col[discarded_indices]
-
-# Count the occurrences of each spatial domain for the discarded spots
-domain_counts <- table(discarded_domains)
-
-# Convert to percentages
-domain_percentages <- (domain_counts / sum(domain_counts)) * 100
-
-# Convert the domain counts to a data frame
-df <- as.data.frame(domain_percentages)
-colnames(df) <- c("SpatialDomain", "Percentage")
-
-# Plot using ggplot2
-p <- ggplot(df, aes(x = SpatialDomain, y = Percentage, fill = SpatialDomain)) +
-  geom_bar(stat = "identity") +
-  scale_fill_manual(values = spatialLIBD::libd_layer_colors) +
-  labs(title = "Percentage of Discarded Spots by Spatial Domain",
-       x = "Spatial Domain",
-       y = "Percentage") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-pdf(width=6, height=3,here(plot_dir, "Barplot_discarded_by_layer_k18.pdf"))
-p
-dev.off()
-
-
-
-
-# ===== Average percentage across samples =====
 # Extract the required columns
 discard_new_col <- spe$z_umi_outlier
 layer_guess_ordered_col <- spe$layer_guess_reordered
@@ -256,47 +176,38 @@ df <- data.frame(discard_new = discard_new_col,
 
 df <- df[!is.na(df$layer_guess_ordered), ]
 
-# Filter out the discarded spots
-discarded_df <- df[df$discard_new == TRUE, ]
 
-# Group by sample_id and layer_guess_ordered, then calculate percentages
-summary_df <- discarded_df %>%
+summary_df <- df %>%
   group_by(sample_id, layer_guess_ordered) %>%
-  summarise(n = n()) %>%
-  mutate(percentage = (n / sum(n)) * 100)
-
-# Create a dataframe with every possible combination of sample_id and layer_guess_ordered
-all_combinations <- df %>% 
-  tidyr::expand(sample_id, layer_guess_ordered)
-
-
-# Left join summary_df onto all_combinations, filling in missing values with 0
-final_df <- left_join(all_combinations, summary_df, by = c("sample_id", "layer_guess_ordered")) %>%
-  replace_na(list(n = 0, percentage = 0))
+  summarise(
+    total_spots = n(),  # Total number of spots in each domain for each sample
+    discarded_spots = sum(discard_new, na.rm = TRUE),  # Counting TRUE as 1 and FALSE as 0
+    .groups = 'keep'  # Dropping the grouping after summarise
+  ) %>%
+  mutate(
+    percentage_discarded = (discarded_spots / total_spots) * 100  # Calculating the percentage
+  ) 
 
 # Plot using ggplot2
-p <- ggplot(final_df, aes(x = layer_guess_ordered, y = percentage, fill = layer_guess_ordered)) +
+p <- ggplot(summary_df, aes(x = layer_guess_ordered, y = percentage_discarded, fill = layer_guess_ordered)) +
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
   geom_jitter(width = 0.2, size = 2, aes(color = layer_guess_ordered), alpha = 0.7) +  # Display all data points with matching layer colors
   scale_fill_manual(values = spatialLIBD::libd_layer_colors) +
   scale_color_manual(values = spatialLIBD::libd_layer_colors) +
-  labs(title = "Percentage of Discarded Spots by Spatial Domain for each Sample ID",
+  labs(title = "Percentage of Discarded Spots within each Spatial Domain",
        x = "Spatial Domain",
        y = "Percentage") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-pdf(width=8, height=4,here(plot_dir, "Boxplot_discarded_by_layer_by_sample_k18.pdf"))
+pdf(width=8, height=4,here(plot_dir, "Boxplot_percent_discarded_k18.pdf"))
 p
 dev.off()
 
 
 
-# ============ Testing different Ks ==========
-# ====================== Testing with my newly made function ======================
 
-# I turned the above code into functions in the private SpotSweeper package
-# https://github.com/MicTott/SpotSweeper
+# ============ Testing different Ks ==========
 
 spe <- localOutliers(spe, k=36 , threshold=3)
 
@@ -307,7 +218,7 @@ spe.subset <- subset(spe, ,sample_id == spe$sample_id[1])
 # Visualize
 p1 <- make_escheR(spe.subset) |> 
   add_fill(var = "sum_umi_log2") +
-  scale_fill_gradient2(low ="purple" , mid = "white",high =  "darkgreen")
+  scale_fill_gradient(low ="white",high =  "darkgreen")
 
 p2 <- make_escheR(spe.subset) |> 
   add_fill(var = "z.umi") +
@@ -322,7 +233,7 @@ p3 <- make_escheR(spe.subset) |>
       "TRUE" = "red",
       "FALSE" = "transparent")
   ) +
-  scale_fill_gradient2(low ="purple" , mid = "white",high =  "darkgreen")
+  scale_fill_gradient(low ="white",high =  "darkgreen")
 
 p4 <- make_escheR(spe.subset) |> 
   add_fill(var = "z.umi") |>
@@ -341,44 +252,7 @@ dev.off()
 
 # ====================== Explore these outliers ======================
 
-
 # ==== Total percentage =====
-# Extract the required columns from the spe object
-discard_new_col <- spe$z_umi_outlier
-layer_guess_ordered_col <- spe$layer_guess_reordered
-
-# Filter out the discarded spots
-discarded_indices <- which(discard_new_col == TRUE)
-discarded_domains <- layer_guess_ordered_col[discarded_indices]
-
-# Count the occurrences of each spatial domain for the discarded spots
-domain_counts <- table(discarded_domains)
-
-# Convert to percentages
-domain_percentages <- (domain_counts / sum(domain_counts)) * 100
-
-# Convert the domain counts to a data frame
-df <- as.data.frame(domain_percentages)
-colnames(df) <- c("SpatialDomain", "Percentage")
-
-# Plot using ggplot2
-p <- ggplot(df, aes(x = SpatialDomain, y = Percentage, fill = SpatialDomain)) +
-  geom_bar(stat = "identity") +
-  scale_fill_manual(values = spatialLIBD::libd_layer_colors) +
-  labs(title = "Percentage of Discarded Spots by Spatial Domain",
-       x = "Spatial Domain",
-       y = "Percentage") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-pdf(width=6, height=3,here(plot_dir, "Barplot_discarded_by_layer_k36.pdf"))
-p
-dev.off()
-
-
-
-
-# ===== Average percentage across samples =====
 # Extract the required columns
 discard_new_col <- spe$z_umi_outlier
 layer_guess_ordered_col <- spe$layer_guess_reordered
@@ -391,90 +265,33 @@ df <- data.frame(discard_new = discard_new_col,
 
 df <- df[!is.na(df$layer_guess_ordered), ]
 
-# Filter out the discarded spots
-discarded_df <- df[df$discard_new == TRUE, ]
 
-# Group by sample_id and layer_guess_ordered, then calculate percentages
-summary_df <- discarded_df %>%
+summary_df <- df %>%
   group_by(sample_id, layer_guess_ordered) %>%
-  summarise(n = n()) %>%
-  mutate(percentage = (n / sum(n)) * 100)
-
-# Create a dataframe with every possible combination of sample_id and layer_guess_ordered
-all_combinations <- df %>% 
-  tidyr::expand(sample_id, layer_guess_ordered)
-
-
-# Left join summary_df onto all_combinations, filling in missing values with 0
-final_df <- left_join(all_combinations, summary_df, by = c("sample_id", "layer_guess_ordered")) %>%
-  replace_na(list(n = 0, percentage = 0))
+  summarise(
+    total_spots = n(),  # Total number of spots in each domain for each sample
+    discarded_spots = sum(discard_new, na.rm = TRUE),  # Counting TRUE as 1 and FALSE as 0
+    .groups = 'keep'  # Dropping the grouping after summarise
+  ) %>%
+  mutate(
+    percentage_discarded = (discarded_spots / total_spots) * 100  # Calculating the percentage
+  ) 
 
 # Plot using ggplot2
-p <- ggplot(final_df, aes(x = layer_guess_ordered, y = percentage, fill = layer_guess_ordered)) +
+p <- ggplot(summary_df, aes(x = layer_guess_ordered, y = percentage_discarded, fill = layer_guess_ordered)) +
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
   geom_jitter(width = 0.2, size = 2, aes(color = layer_guess_ordered), alpha = 0.7) +  # Display all data points with matching layer colors
   scale_fill_manual(values = spatialLIBD::libd_layer_colors) +
   scale_color_manual(values = spatialLIBD::libd_layer_colors) +
-  labs(title = "Percentage of Discarded Spots by Spatial Domain for each Sample ID",
+  labs(title = "Percentage of Discarded Spots within each Spatial Domain",
        x = "Spatial Domain",
        y = "Percentage") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-pdf(width=8, height=4,here(plot_dir, "Boxplot_discarded_by_layer_by_sample_k36.pdf"))
+pdf(width=8, height=4,here(plot_dir, "Boxplot_percent_discarded_k36.pdf"))
 p
 dev.off()
 
-
-
-
-# =========== Plot the variance per layer ============
-# Extract the required columns
-discard_new_col <- spe$z_umi_outlier
-layer_guess_ordered_col <- spe$layer_guess_reordered
-sample_id_col <- spe$sample_id
-variance_col <- spe$var.umi
-
-
-# Create a data frame
-df <- data.frame(discard_new = discard_new_col,
-                 layer_guess_ordered = layer_guess_ordered_col,
-                 sample_id = sample_id_col,
-                 variance = variance_col)
-
-df <- df[!is.na(df$layer_guess_ordered), ]
-
-
-# Group by sample_id and layer_guess_ordered, then calculate percentages
-summary_df <- df %>%
-  group_by(sample_id, layer_guess_ordered) %>%
-  summarise(n = n(),
-            mean_variance = mean(variance),
-            sd_variance = sd(variance))
-
-# Create a dataframe with every possible combination of sample_id and layer_guess_ordered
-all_combinations <- df %>% 
-  tidyr::expand(sample_id, layer_guess_ordered)
-
-
-# Left join summary_df onto all_combinations, filling in missing values with 0
-final_df <- left_join(all_combinations, summary_df, by = c("sample_id", "layer_guess_ordered")) %>%
-  replace_na(list(n = 0, mean_variance = 0, sd_variance = 0))
-
-# Plot using ggplot2
-p <- ggplot(final_df, aes(x = layer_guess_ordered, y = mean_variance, fill = layer_guess_ordered)) +
-  geom_boxplot(alpha = 0.7, outlier.shape = NA) +
-  geom_jitter(width = 0.2, size = 2, aes(color = layer_guess_ordered), alpha = 0.7) +  # Display all data points with matching layer colors
-  scale_fill_manual(values = spatialLIBD::libd_layer_colors) +
-  scale_color_manual(values = spatialLIBD::libd_layer_colors) +
-  labs(title = "Mean Variance by Spatial Domain for each Sample ID",
-       x = "Spatial Domain",
-       y = "Mean Variance") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-pdf(width=8, height=4,here(plot_dir, "Boxplot_mean_variance_by_layer_by_sample_k36.pdf"))
-p
-dev.off()
 
 
